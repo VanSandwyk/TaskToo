0b11df3083021694e2185b04c2b7f44341bed999
vanSandwyk <van.sandwyk43@gmail.com>
2023-04-18 12:16:26 +0200

    ava application to only output user-selected fields from that XML file.

--- app/src/main/java/sdp/prac2/task2/Main.java
+++ app/src/main/java/sdp/prac2/task2/Main.java
@@ -1,44 +1,49 @@
 package sdp.prac2.task2;
 
-import java.io.File;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            File file = new File("src/main/resources/Data.xml");
+            File inputFile = new File("data.xml");
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(file);
+            Document doc = dBuilder.parse(inputFile);
             doc.getDocumentElement().normalize();
-
             NodeList nodeList = doc.getElementsByTagName("record");
-
             for (int i = 0; i < nodeList.getLength(); i++) {
-                Element element = (Element) nodeList.item(i);
-                String name = element.getElementsByTagName("name").item(0).getTextContent();
-                String postalZip = element.getElementsByTagName("postalZip").item(0).getTextContent();
-                String region = element.getElementsByTagName("region").item(0).getTextContent();
-                String country = element.getElementsByTagName("country").item(0).getTextContent();
-                String address = element.getElementsByTagName("address").item(0).getTextContent();
-                String list = element.getElementsByTagName("list").item(0).getTextContent();
-
-                System.out.println("Record " + (i+1) + ":");
-                System.out.println("Name: " + name);
-                System.out.println("Postal Zip: " + postalZip);
-                System.out.println("Region: " + region);
-                System.out.println("Country: " + country);
-                System.out.println("Address: " + address);
-                System.out.println("List: " + list);
-                System.out.println();
+                Node node = nodeList.item(i);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) node;
+                    String name = "";
+                    String country = "";
+                    // check if name and country fields are selected
+                    if (args.length >= 1 && args[0].equals("name")) {
+                        name = eElement.getElementsByTagName("name").item(0).getTextContent();
+                    }
+                    if (args.length >= 2 && args[1].equals("country")) {
+                        country = eElement.getElementsByTagName("country").item(0).getTextContent();
+                    }
+                    // print selected fields
+                    System.out.println("Record " + (i+1) + ":");
+                    if (!name.isEmpty()) {
+                        System.out.println("Name: " + name);
+                    }
+                    if (!country.isEmpty()) {
+                        System.out.println("Country: " + country);
+                    }
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
+
 
